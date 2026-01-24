0dbd33dced0d72df9d7cdf7420d746d1bca20e52
cszsworks <cszsworks@gmail.com>
2026-01-24 11:33:26 +0100

    Pdf

--- src/main/java/com/cszsworks/persistence/JSONToSQL.java
+++ src/main/java/com/cszsworks/persistence/JSONToSQL.java
@@ -9,37 +9,42 @@ import java.sql.PreparedStatement;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+//PreparedStatement használat JDBCből
 public class JSONToSQL {
-
+    //adatbázis kapcsolat... majd átkerül .env-be
     private static final String DB_URL = "jdbc:mysql://sql7.freesqldatabase.com:3306/sql7814323";
     private static final String DB_USER = "sql7814323";
     private static final String DB_PASS = "W3Ey4JZ2ae";
 
     public static void exportJSONToSQL(String jsonFilePath) {
         try {
+            //json fajl stringbe
             String content = new String(Files.readAllBytes(Paths.get(jsonFilePath)));
+            //string jsonként azonosítása
             JSONArray array = new JSONArray(content);
-
+            //adatbázis kapcsolat
             try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS)) {
                 //az ignore a duplicate kihagyás username,highscore,updated_at-re
                 String sql = "INSERT IGNORE INTO highscores (username, highscore, updated_at) VALUES (?, ?, ?)";
                 //előre megírt SQL query
                 PreparedStatement stmt = conn.prepareStatement(sql);
-
+                //Dátum formátum konvertlás
                 DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
-
+                // JSON tömb feldolgozása
                 for (int i = 0; i < array.length(); i++) {
+                    //a tömb darabjai objektumként
                     JSONObject obj = array.getJSONObject(i);
                     //begyüjti az adatokat a json keyektől
                     String playerName = obj.getString("playerName");
                     int score = obj.getInt("score");
                     LocalDateTime playedAt = LocalDateTime.parse(obj.getString("playedAt"), formatter)
-                            .withNano(0);
+                            .withNano(0);  //nanosecet nem szereti sql
 
                     //sorrendben pótoljuk a ?-eket
                     stmt.setString(1, playerName);
                     stmt.setInt(2, score);
                     stmt.setObject(3, playedAt);
+                    //sql végrehajtás
                     stmt.executeUpdate();
                 }
             }
--- src/main/java/com/cszsworks/persistence/SQLToJSON.java
+++ src/main/java/com/cszsworks/persistence/SQLToJSON.java
@@ -11,8 +11,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.time.format.DateTimeFormatter;
 
-
+//PreparedStatement használat JDBCből
 public class SQLToJSON {
+    //adatbázis kapcsolat adatok
     private static final String DB_URL = "jdbc:mysql://sql7.freesqldatabase.com:3306/sql7814323";
     private static final String DB_USER = "sql7814323";
     private static final String DB_PASS = "W3Ey4JZ2ae";
@@ -21,15 +22,17 @@ public class SQLToJSON {
         JSONArray array = new JSONArray();
 
         try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS)) {
-
+            //preparedstatement lekrédezés futtatása
             String sql = "SELECT username, highscore, updated_at FROM highscores";
             PreparedStatement stmt = conn.prepareStatement(sql);
             ResultSet rs = stmt.executeQuery();
 
             DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
-
+            //beolvasó "kurzor" mozog a tartalomban
             while (rs.next()) {
+                //minden ugrás egy objektum, egészen a végéig
                 JSONObject obj = new JSONObject();
+                //json objektum megtöltése a mezők visszaolvasása szeirnt
                 obj.put("playerName", rs.getString("username"));
                 obj.put("score", rs.getInt("highscore"));
                 obj.put(
@@ -39,7 +42,7 @@ public class SQLToJSON {
 
                 array.put(obj);
             }
-
+            //visszaírja json fájlba a tartalmat
             Files.write(
                     Paths.get(jsonFilePath),
                     array.toString(4).getBytes()
